8c701bd722cd52ad78eaf579cf28d29e32f7153f
EuJonass <111382334+EuJonass@users.noreply.github.com>
2022-10-13 11:16:22 -0300

    Add files via upload

---
+++ Mundo 01 - python/y - Comentarios TikTok.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+
+while True:
+    time.sleep(1)
+    pyautogui.click(x=818, y=741)
+    time.sleep(1)
+    pyautogui.click(x=944, y=684)
+    pyautogui.write("@jot4_7bb")
+    time.sleep(1)
+    pyautogui.click(x=1310, y=673)
+    time.sleep(1)
+
