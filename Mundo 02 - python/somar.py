0056d9d7f4a748e53f5fe7ad7f48c0917425c44b
EuJonass <111382334+EuJonass@users.noreply.github.com>
2022-10-13 11:32:03 -0300

    Add files via upload

---
+++ Mundo 02 - python/somar.py
@@ -0,0 +1,10 @@
+def somar():
+    a = 18 + 96
+    print('faremos a soma')
+    print(f'A soma entre 18 e 96 é {a}')
+
+
+def diminuir():
+    b = 96 - 18
+    print('Faremos a subtração')
+    print(f'A subtração entre 96 - 18 é {b}')
