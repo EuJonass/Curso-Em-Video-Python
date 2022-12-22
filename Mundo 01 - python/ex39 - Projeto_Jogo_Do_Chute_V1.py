b7667620f69482ca955f0d197a54e323bc37a80f
EuJonass <111382334+EuJonass@users.noreply.github.com>
2022-12-22 13:15:09 -0300

    Update ex39 - Projeto_Jogo_Do_Chute_V1.py

--- Mundo 01 - python/ex39 - Projeto_Jogo_Do_Chute_V1.py
+++ Mundo 01 - python/ex39 - Projeto_Jogo_Do_Chute_V1.py
@@ -7,15 +7,16 @@ print("=-=-" * 40)
 l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 escolha = random.choice(l)
 
-n = int(input("Digite um número:"))
+while True:
+    n = int(input("Digite um número:"))
 
-if n == escolha:
+    if n == escolha:
         print("VOCÊ ACERTOU PARÁBENS :), o número escolhido foi {}!!".format(escolha))
+        break
 
-elif n > escolha:
+    elif n < escolha:
         print("Chute um número maior :)")
-else:
+    else:
         print("Chute um número menor :)")
 
-n = int(input("Digite um número:"))
 
