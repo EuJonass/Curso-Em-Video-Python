c899ea9efb763a2e703133993e9adbde27cdddbb
EuJonass <111382334+EuJonass@users.noreply.github.com>
2022-12-23 13:32:44 -0300

    Update ex17 - Projeto_Catetos_&_Hipotenusa.py

--- Mundo 01 - python/ex17 - Projeto_Catetos_&_Hipotenusa.py
+++ Mundo 01 - python/ex17 - Projeto_Catetos_&_Hipotenusa.py
@@ -1,10 +1,12 @@
 from math import hypot
-'''co = float(input('Entre com o valor do cateto oposto: '))
+'''
+co = float(input('Entre com o valor do cateto oposto: '))
 ca = float(input('Entre com o valor do cateto adjacente: '))
 hi= (ca ** 2 + co ** 2) ** (1/2)
 
 print("\n**************************\n")
-print("O valor da hipotenusa é: {:.2f}".format(hi))'''
+print("O valor da hipotenusa é: {:.2f}".format(hi))
+'''
 
 c1 = float(input("Entre com o valor do cateto oposto: "))
 c2 = float(input("Entre com o valor do cateto adjacente: "))
